3c2083fd88b8d6719ee4915cabd789b96ead3f31
Lojnik <loj210680@spengergasse.at>
2026-05-29 22:26:30 +0200

    awawwaawaw Im so silly T^T

--- src/main/java/com/porcelanrose/neokiss/SneakData.java
+++ src/main/java/com/porcelanrose/neokiss/SneakData.java
@@ -1,11 +1,11 @@
 package com.porcelanrose.neokiss;
 
-private static class NeoKiss.SneakData {
+private static class SneakData {
     int count = 0;
     long lastTime = -80L;
     int particleLevel = 1;
     boolean wasSneaking = false;
 
-    private Neokiss.SneakData() {
+    private SneakData() {
     }
 }
